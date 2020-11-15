d5285deb15d8defc479428e1670165084d7ceb1b
natchelm <natchel2003@yahoo.com>
2020-11-15 18:05:23 +0800

    second

---
+++ js/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(window) {
+	var byeSpeaker = {};
+	byeSpeaker.speakWord = "Goodbye";
+	byeSpeaker.speak = function(name) {
+	  console.log(byeSpeaker.speakWord + " " + name);
+	}
+	window.byeSpeaker = byeSpeaker;
+})(window);
